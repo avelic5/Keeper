2fb788d7342e8621ad9eb6ab6deaeec3bd136d63
avelic5 <avelic5@etf.unsa.ba>
2025-09-29 20:49:09 +0200

    Mapping data to components, add static data and use react props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../data/notes";
+
 
 export default function App() {
     
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(el=><Note key={el.key} title={el.title} content={el.content}/>)}
       <Footer />
     </div>
   );
