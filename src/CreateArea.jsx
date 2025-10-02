c866b0fd1822b444bf005cc69bc49d93b7eefcde
avelic5 <avelic5@etf.unsa.ba>
2025-10-02 13:56:52 +0200

    Add some style to app

--- src/CreateArea.jsx
+++ src/CreateArea.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
+import AddIcon from "@mui/icons-material/Add";
+import { Fab } from "@mui/material";
+import { Zoom } from "@mui/material";
 
 function CreateArea(props) {
+    const [zoomState,setZoomState] =useState(false);
+    const [rowState,setRowState] = useState(1);
+    function onClick(){
+      setRowState(3);
+      setZoomState(true);  
+    }
   return (
     <div>
-      <form>
-        <input onChange={(event)=>props.setHeading(event.target.value)} name="title" placeholder="Title" value={props.heading} />
-        <textarea onChange={(event)=>props.setTextarea(event.target.value)} name="content" placeholder="Take a note..." rows="3" value={props.textarea}/>
-        <button type="button" onClick={props.onClick}>Add</button>
+      <form className="create-note">
+        <input hidden={!zoomState} onChange={(event)=>props.setHeading(event.target.value)} name="title" placeholder="Title" value={props.heading} />
+        <textarea onClick={onClick} onChange={(event)=>props.setTextarea(event.target.value)} name="content" placeholder="Take a note..." rows={rowState} value={props.textarea}/>
+        <Zoom in={zoomState}><Fab type="button" onClick={()=>{props.onClick(); setRowState(1);setZoomState(false);}}><AddIcon /></Fab></Zoom>
       </form>
     </div>
   );
